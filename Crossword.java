81c36e58c56853bec1614fe91153e1d84b77e370
Garrett Hagen <garretthagen21@gmail.com>
2018-09-24 12:15:00 -0400

    Finished recursive piece. Working on safe method

--- Crossword.java
+++ Crossword.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Crossword{
+		static int solutionNumber = 0;
 		static int boardSize;
 		static StringBuilder rowStr [];
 		static StringBuilder colStr [];
@@ -53,51 +54,178 @@ public class Crossword{
 		
 	
 	public static void buildCrosswordPuzzle(int i,int j,int endIndex) {
-			
-			//We have reached the end of the puzzle
-			if(j == endIndex && i==endIndex){
-				printGameboard();
-				System.exit(0);
-			}
+			char boardChar = gameBoard[i][j];
 			
 			for(int l=0;l<26;l++) {
-				//i is not an end index so colStr[j]+alphabet[l] must be ATLEAST a prefix
-				if (i < endIndex) {
-					if(theDictionary.searchPrefix(colStr[j].append(alphabet[l])) > 0) {
-						System.out.println(alphabet[l]);
-						buildCrosswordPuzzle(i+1,j+1,endIndex);
-					}
-					else {
-						colStr[j].deleteCharAt(colStr[j].length()-1);
-					}
+				
+				//Attempt to place the letter
+				if(boardChar == '+') {
+					colStr[j].append(alphabet[l]);
+					rowStr[i].append(alphabet[l]);
 				}
-				//i is an end index so colStr[j]+alphabet[l] must be a word
-				else if(i == endIndex){
-					if(theDictionary.searchPrefix(colStr[j].append(alphabet[l])) > 1) {
-						buildCrosswordPuzzle(i,j+1,endIndex);
-					}
-					else {
-						colStr[j].deleteCharAt(colStr[j].length()-1);
-					}
+				else {
+					colStr[j].append(boardChar);
+					rowStr[i].append(boardChar);
+				}
+				
+				
+				//All of our conditions have been met so the character placement is valid. Recurses
+				if(safe(i,j,endIndex)) {	
+					
+					int iNext = 0;
+					int jNext = 0;
+					//Solution found
+					if(j==endIndex && i==endIndex){
+						printGameboard();
+						//System.exit(0);
+					}	
+					
+					else { //Reached the end of the row (i.e used all columns) so move to the next row and reset j
+						if(j==endIndex) {
+							iNext = i+1;
+							jNext = 0;
+						}
+						else { 
+							iNext = i;
+							jNext = j+1;	
+						}
+						buildCrosswordPuzzle(iNext,jNext,endIndex);
+					}	
+				}
+				//Either we found a solution, we are backtracking, or the letter we added is invalid
+				rowStr[i].deleteCharAt(j);
+			    colStr[j].deleteCharAt(i);
+				
 			}
+			return;
+		}
+	private static boolean safe(int i, int j,int endIndex) {	
+		StringBuilder rowString;
+		StringBuilder colString;
+		int colStart = 0,colEnd = i;
+		int rowStart = 0,rowEnd = j;
+		
+		
+		//Substring is (inclusive,exclusive) so we want to omit the dash when we build our string to check
+		if(colStr[j].charAt(i) != '-')
+			colEnd++;
+			rowEnd++; 
+		
+		for(int start = i; start>-1;start--) {
+			if(colStr[j].charAt(start) == '-' && start != i) {
+					colStart = start+1;
+					break;
+				}			
+		}
+		for(int start = j; start>-1;start--) {
+			if(rowStr[i].charAt(start) == '-' && start != j) {
+					rowStart = start+1;
+					break;
+				}			
+		}
+		rowString = new StringBuilder(rowStr[i].substring(rowStart,rowEnd));
+		colString = new StringBuilder(colStr[i].substring(colStart,colEnd));
+		System.out.println(rowString.toString());
+		System.out.println(colString.toString());
+		
+		if (i < endIndex){ 
+			/*if(colStr[j].charAt(i) == '-') {
+				int startingDash = 0;
+				//Start at the current dash and work backwards til we see another dash
+				for(int q = i; q>-1;q--) {
+					if(colStr[j].charAt(q) == '-')
+						startingDash = q;
+						break;
+				}
+				String subWord = colStr[j].substring(startingDash,i);
+				System.out.print(subWord+":");
+				if(!subWord.equals("")){
+						if(!(theDictionary.searchPrefix(new StringBuilder(subWord)) > 1)){
+							System.out.print("FAILED\n");
+							return false;			
+						}
+				    }
+				System.out.print("PASSED\n");
+				}*/
+			if(theDictionary.searchPrefix(colString) < 1){
+				return false;
+			}
+		}
+		if (j < endIndex){ 
+			/*if(rowStr[i].charAt(j) == '-') {
+				int startingDash = 0;
+				//Start at the current dash and work backwards til we see another dash
+				for(int q = j; q>-1;q--) {
+					if(rowStr[i].charAt(q) == '-')
+						startingDash = q;
+						break;
+				}
+
+				String subWord = rowStr[i].substring(startingDash,j);
+				System.out.print(subWord+":");
+				if(!subWord.equals("")){
+						if(!(theDictionary.searchPrefix(new StringBuilder(subWord)) > 1)){
+							System.out.print("FAILED\n");
+							return false;
+						}
+				    }
+				System.out.print("PASSED\n");
+
+				}*/
+			if(theDictionary.searchPrefix(rowString) < 1){
+				return false;
+			}
+		}
+	
+		if(j == endIndex){
+			/*int recentDash = rowStr[i].lastIndexOf("-");
+			if(recentDash != -1) {
+				String subWord = rowStr[i].substring(recentDash,endIndex);
+				System.out.print(subWord+":");
+				if(!subWord.equals("")){
+						if(!(theDictionary.searchPrefix(new StringBuilder(subWord)) > 1)){
+							System.out.print("FAILED\n");
+							return false;
+						}
+				    }
+				System.out.print("PASSED\n");
+				}*/
+			if(!(theDictionary.searchPrefix(rowString) > 1)) { 
+				return false;
+			}
+				
 			
 		}
-		return;
-		
-		
-	}
-	private static boolean validCol(char letter,int j) {
-		//if(theDictionary.searchPrefix)
-	return true;
+		if(i == endIndex){
+			/*int recentDash = colStr[j].lastIndexOf("-");
+			if(recentDash != -1) {
+				String subWord = colStr[j].substring(recentDash,endIndex);
+				System.out.print(subWord+":");
+				if(!subWord.equals("")){
+						if(!(theDictionary.searchPrefix(new StringBuilder(subWord)) > 1)){
+							System.out.print("FAILED\n");
+							return false;
+						}
+				    }	
+				System.out.print("PASSED\n");
+				}*/
+			if(!(theDictionary.searchPrefix(colString) > 1)) { 
+				return false;
+			}
+				
+			
+		}
+		return true;
 	}
 	private static void printGameboard() {
+		solutionNumber++;
+		System.out.println("\n------------ Solution #"+solutionNumber+" ------------\n");
 		for(int i = 0;i<boardSize;i++) {
 			System.out.println();
 			for(int j = 0;j<boardSize;j++) {
 				System.out.print(rowStr[i].charAt(j)+" ");
 			}
 		}
-		
 	}
 	
 	
